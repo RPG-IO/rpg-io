e433715a2d73ea01565831c2adef50d94136cc38
Kacper Kafara <kacperkafara@gmail.com>
2022-03-26 15:59:21 +0100

    chore: Create initial setup for Java app (#2)
    * chore: Creat .gitignore file
    
    * chore: Initial setup (JavaFX app)
    
    * fix: Make the project compile
    
    * chore: Setup project build on CI (#3)
    
    * chore: Setup project build on CI
    
    * chore: Update CI build script
    
    * chore: Configure CI build
    
    * chore: Change java vendor on CI
    
    * chore: Use gradle cache on CI
    
    * chore: Remove comments from gradle
    
    * chore: Rename package io.rpg.rpg -> io.rpg
    
    * chore: Upgrade JavaFX version to latest LTS
    
    * Upgrade gradle version to lates release

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module io.rpg.rpg {
+  requires javafx.controls;
+  requires javafx.fxml;
+  requires javafx.web;
+
+  requires org.controlsfx.controls;
+  requires com.dlsc.formsfx;
+  requires org.kordamp.ikonli.javafx;
+  requires org.kordamp.bootstrapfx.core;
+
+  opens io.rpg to javafx.fxml;
+  exports io.rpg;
+}
