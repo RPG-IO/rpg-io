1bd637bd5bfd2948211a19f0f38a1a25fc0fdc75
Kacper Kafara <kacperkafara@gmail.com>
2022-03-29 19:37:47 +0200

    feat: Setup logging in application entry point

--- src/main/java/io/rpg/HelloApplication.java
+++ src/main/java/io/rpg/HelloApplication.java
@@ -4,10 +4,24 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+  // As start method is called even before main function, we need
+  // to initialize logging even earlier
+  @Override
+  public void init() {
+    setupLogging();
+
+    Logger logger = LogManager.getLogger(HelloApplication.class);
+    logger.info("Running application...");
+  }
+
   @Override
   public void start(Stage stage) throws IOException {
     FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
@@ -15,6 +29,11 @@ public class HelloApplication extends Application {
     stage.setTitle("Hello!");
     stage.setScene(scene);
     stage.show();
+  }
+
+  public static void setupLogging() {
+    // log everything
+    Configurator.setRootLevel(Level.TRACE);
   }
 
   public static void main(String[] args) {
