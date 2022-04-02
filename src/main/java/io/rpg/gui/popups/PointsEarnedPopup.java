901e572d1081eec1785e4b9318d0e4d3e1889813
mhawryluk <marcinhawrylukxx@gmail.com>
2022-04-02 19:18:18 +0200

    chore: Add background image and improve styling of points popup

--- src/main/java/io/rpg/gui/popups/PointsEarnedPopup.java
+++ src/main/java/io/rpg/gui/popups/PointsEarnedPopup.java
@@ -7,6 +7,8 @@ import javafx.scene.paint.Color;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.stage.Window;
+import javafx.util.Pair;
 
 import java.io.IOException;
 import java.util.Objects;
@@ -25,14 +27,19 @@ public class PointsEarnedPopup {
         }
 
         Stage popupStage = new Stage(StageStyle.TRANSPARENT);
-        popupStage.initOwner(scene.getWindow());
-        popupStage.initModality(Modality.APPLICATION_MODAL);
-
-        popupStage.setScene(new Scene(root, Color.TRANSPARENT));
+        Window window = scene.getWindow();
+        popupStage.initOwner(window);
+//        popupStage.initModality(Modality.APPLICATION_MODAL);
 
         PointsPopupController controller = loader.getController();
         controller.setPointsCount(pointsCount);
+        Pair<Double, Double> backgroundDims = controller.setBackgroundImage("file:assets/point-popup-bg.png");
 
+        popupStage.setScene(new Scene(root, Color.TRANSPARENT));
+
+
+        popupStage.setX(window.getX() + window.getWidth()/2 - backgroundDims.getKey()/2);
+        popupStage.setY(window.getY() + window.getHeight()/2 - backgroundDims.getValue()/2);
         popupStage.show();
 
         popupStage.focusedProperty().addListener((obs, wasFocused, isNowFocused) -> {
--- src/main/java/io/rpg/gui/popups/PointsPopupController.java
+++ src/main/java/io/rpg/gui/popups/PointsPopupController.java
@@ -2,13 +2,27 @@ package io.rpg.gui.popups;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.util.Pair;
 
 public class PointsPopupController {
 
-    @FXML
-    private Label label;
+    @FXML private Label label;
+    @FXML private Pane background;
 
     protected void setPointsCount(int pointsCount) {
         label.setText("Earned " + pointsCount + " points");
+    }
+
+    protected Pair<Double, Double> setBackgroundImage(String url){
+        BackgroundImage backgroundImg= new BackgroundImage(
+                new Image(url),
+                BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.DEFAULT,
+                BackgroundSize.DEFAULT
+        );
+        background.setBackground(new Background(backgroundImg));
+        return new Pair<>(background.getPrefWidth(), background.getPrefHeight());
     }
 }
