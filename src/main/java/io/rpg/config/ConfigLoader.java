35ba9c9a76c8711ad1f4d2540b17aca5a54c4541
Kacper Kafara <kacperkafara@gmail.com>
2022-04-02 11:06:55 +0200

    refract: Change String paths to java.nio classes

--- src/main/java/io/rpg/config/ConfigLoader.java
+++ src/main/java/io/rpg/config/ConfigLoader.java
@@ -3,16 +3,33 @@ package io.rpg.config;
 import com.google.gson.Gson;
 import org.jetbrains.annotations.NotNull;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class ConfigLoader {
   private final Gson gson;
-  private final String pathToConfigDir;
+  private final Path pathToConfigDir;
+  private final Path pathToRootFile;
+
 
   public ConfigLoader(@NotNull String configDirPath) {
-    pathToConfigDir = configDirPath;
+
+    pathToConfigDir = Path.of(configDirPath);
+    pathToRootFile = pathToConfigDir.resolve(ConfigConstants.ROOT);
     gson = new Gson();
+
+    validateState();
   }
 
   public void load() {
 
+  }
+
+  void validateState() {
+    if (!Files.isDirectory(pathToConfigDir)) {
+
+      throw new IllegalArgumentException("Could not resolve " + pathToConfigDir + " directory. " +
+          "Make sure that the config dir path is correct.");
+    }
   }
 }
