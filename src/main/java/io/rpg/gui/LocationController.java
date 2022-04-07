497231a104efcf275dcad8682255d7e31ad5fc41
co012 <co123@interia.eu>
2022-04-07 18:02:34 +0200

    @co012/RPG-93 widac lokacje w okienku (#8)
    * chore: Init gui
    
    * feat: now location is centered
    
    * RPG-79 Model proposal & configuration loading (#6)
    
    * chore: Add dependency on Gson
    
    This library is required, as our configuration files will be based on
    json
    
    * chore: Add dependency on jetbrains.annotations
    
    * fix: Change package name in module-info.java
    
    * chore: Push out commit
    
    * feat: Extend class schema
    
    * refract: Rename DialogicGameObject -> DialogGameObject
    
    * feat: Add Position record
    
    * feat: Initial GameObject implementation
    
    * refract: Make GameWorld class final
    
    * feat: Add empty Location class
    
    * docs: Add docs dir & empty configuration description
    
    * chore: Add ConfigConstants class with static data members
    
    * refract: Resign from m prefix in private member var names
    
    * chore: Add dependency on log4j
    
    * fix: Fix module-info: export io.rpg
    
    * feat: Setup logging in application entry point
    
    * chore: Package structure
    
    * refract: Empty line at the end of build.gradle
    
    * feat: Initlial configuration structure
    
    * refract: Code cleanup
    
    * refract: Change ROOT file name to root.json
    
    * refract: Change String paths to java.nio classes
    
    * chore: Add logging to ConfigLoader
    
    * feat: Handle FNF config dir & root file
    
    * chore: Exclude bin directory from repo
    
    * feat: Update configuration structure
    
    * feat: LocationConfig
    
    * feat: GameWorldConfig
    
    * fix: Open io.rpg.model module to gson
    
    * TO REVERT: modify app entry class due to some issues with sdk
    
    * chore: Add mock top level class of application
    
    * feat: Initial ConfigLoader impl
    
    * feat: toString method for GameWorldConfig
    
    * feat: Log GameWorldConfig after deserialization
    
    * chore: Add mock game configuration for unit-testing
    
    * chore: Expose model classes to Gson
    
    * Fix: Adjustments to make Gson work
    
    * feat: ConfigLoader#loadLocation
    
    * test: Add initial tests for ConfigLoader
    
    * test: Add ConfigLoaderTest#LocationConfigIsLoadedProperly test
    
    * chore: Update MockTest
    
    * chore: Update configuration files
    
    * chore: Remove not-up-to-date test
    
    * chore: Improve ConfigLoader#load method
    
    * chore: Improve model classes
    
    * refract: Fix typos
    
    Co-authored-by: adam <adam.przywieczerski@gmail.com>
    
    Co-authored-by: Kacper Kafara <kacperkafara@gmail.com>
    Co-authored-by: adam <adam.przywieczerski@gmail.com>

---
+++ src/main/java/io/rpg/gui/LocationController.java
@@ -0,0 +1,75 @@
+package io.rpg.gui;
+
+import io.rpg.gui.model.LocationModel;
+import io.rpg.model.GameObjectStandIn;
+import javafx.event.EventType;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LocationController implements Initializable {
+    private final static URL FXML_URL = LocationController.class.getResource("location-view.fxml");
+
+    @FXML private ImageView mapImageView;
+    @FXML private Pane foregroundPane, contentPane;
+    @FXML private HBox parent;
+
+    private LocationModel model;
+    private Scene scene;
+
+    public static LocationController load() throws IOException {
+        FXMLLoader loader = new FXMLLoader(FXML_URL);
+        loader.load();
+        return loader.getController();
+    }
+
+    public LocationController() {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        mapImageView.imageProperty().addListener((property, oldImg, newImg) -> {
+            contentPane.setPrefWidth(newImg.getWidth());
+            contentPane.setPrefHeight(newImg.getHeight());
+
+            foregroundPane.setPrefWidth(newImg.getWidth());
+            foregroundPane.setPrefHeight(newImg.getHeight());
+
+            mapImageView.setFitWidth(newImg.getWidth());
+            mapImageView.setFitHeight(newImg.getHeight());
+
+        });
+
+        model = new LocationModel(mapImageView.imageProperty(), foregroundPane.getChildren(), this);
+
+        scene = new Scene(parent);
+        scene.addEventFilter(KeyEvent.KEY_TYPED, this::onKeyTyped);
+    }
+
+    public void onKeyTyped(KeyEvent event) {
+        // TODO: 01.04.2022 Implement key actions
+
+        System.out.println(event);
+
+    }
+
+    public LocationModel getModel(){
+        return model;
+    }
+
+    public Scene getScene() {
+        return scene;
+    }
+}
