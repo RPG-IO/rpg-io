4a27c402f288c9a26b8f679d4b40a0163a26a3a7
Kacper Kafara <kacperkafara@gmail.com>
2022-04-13 16:21:42 +0200

    feat: add builder to the Controller

--- src/main/java/io/rpg/controller/Controller.java
+++ src/main/java/io/rpg/controller/Controller.java
@@ -1,18 +1,20 @@
 package io.rpg.controller;
 
 import io.rpg.model.location.LocationModel;
+import io.rpg.util.Result;
 import io.rpg.view.GameObjectView;
-import io.rpg.view.IOnClickedListener;
-import io.rpg.view.IOnKeyPressedListener;
+import io.rpg.view.IOnClickedObserver;
+import io.rpg.view.IOnKeyPressedObserver;
 import io.rpg.view.LocationView;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.LinkedHashMap;
 
-public class Controller implements IOnClickedListener, IOnKeyPressedListener {
+public class Controller implements IOnClickedObserver, IOnKeyPressedObserver {
   private Scene currentView;
   private LinkedHashMap<String, LocationModel> tagToLocationModelMap;
   private LocationModel currentModel;
@@ -40,7 +42,7 @@ public class Controller implements IOnClickedListener, IOnKeyPressedListener {
     this.currentView = this.tagToLocationViewMap.get(rootTag);
   }
 
-  public void setModel(LocationModel model) {
+  public void setModel(@NotNull LocationModel model) {
     this.tagToLocationModelMap.put(model.getTag(), model);
     currentModel = model;
   }
@@ -61,6 +63,14 @@ public class Controller implements IOnClickedListener, IOnKeyPressedListener {
     this.tagToLocationViewMap = tagToLocationViewMap;
   }
 
+  public LinkedHashMap<String, LocationModel> getTagToLocationModelMap() {
+    return tagToLocationModelMap;
+  }
+
+  public LinkedHashMap<String, LocationView> getTagToLocationViewMap() {
+    return tagToLocationViewMap;
+  }
+
   @Override
   public void onClick(GameObjectView source) {
     // TODO
@@ -71,5 +81,79 @@ public class Controller implements IOnClickedListener, IOnKeyPressedListener {
   public void onKeyPressed(Scene source, KeyEvent event) {
     // TODO
     logger.info("Controller notified on key pressed from " + source);
+  }
+
+  public Result<Controller, Exception> validate() {
+    if (tagToLocationModelMap.size() == 0)
+      return Result.error(new Exception("Empty tag to location model map!"));
+    else if (tagToLocationViewMap.size() == 0)
+      return Result.error(new Exception("Empty tag to location view map!"));
+    else if (tagToLocationViewMap.size() != tagToLocationModelMap.size())
+      return Result.error(new Exception("Mismatched sizes of maps!"));
+    else if (currentView == null)
+      return Result.error(new Exception("No current view set!"));
+    else if (currentModel ==  null)
+      return Result.error(new Exception("No current model set!"));
+    else
+      return Result.ok(this);
+  }
+
+  public static class Builder {
+    private final Controller controller;
+    private boolean isViewSet = false;
+    private boolean isModelSet = false;
+
+    public Builder() {
+      controller = new Controller();
+    }
+
+    public Builder setTagToLocationModelMap(LinkedHashMap<String, LocationModel> tagToLocationModelMap) {
+      controller.setTagToLocationModelMap(tagToLocationModelMap);
+      return this;
+    }
+
+    public Builder setTagToLocationViewMap(LinkedHashMap<String, LocationView> tagToLocationViewMap) {
+      controller.setTagToLocationViewMap(tagToLocationViewMap);
+      return this;
+    }
+
+    public Builder setModel(@NotNull LocationModel model) {
+      if (!isModelSet) {
+        isModelSet = true;
+        controller.setModel(model);
+        return this;
+      } else {
+        throw new IllegalStateException("Attempt to set model for the second time!");
+      }
+    }
+
+    public Builder setView(Scene currentView) {
+      if (!isViewSet) {
+        isViewSet = true;
+        controller.setView(currentView);
+        return this;
+      } else {
+        throw new IllegalStateException("Attempt to set view for the second time!");
+      }
+    }
+
+    public Controller build() {
+      Result<Controller, Exception> validationResult = controller.validate();
+      if (validationResult.isError()) {
+        throw new IllegalStateException(validationResult.getErrorValue());
+      }
+      return controller;
+    }
+
+    public Builder addViewForTag(String tag, LocationView view) {
+      controller.getTagToLocationViewMap().put(tag, view);
+      view.addListener(controller);
+      return this;
+    }
+
+    public Builder addModelForTag(String tag, LocationModel model) {
+      controller.getTagToLocationModelMap().put(tag, model);
+      return this;
+    }
   }
 }
