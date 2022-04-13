de086eb89b8df269003be2395c15ac76b3d7768d
Kacper Kafara <kacperkafara@gmail.com>
2022-04-13 12:13:23 +0200

    chore: make ConfigLoader#{loadGameWorldConfig,loadLocationConfig} methods private

--- src/main/java/io/rpg/config/ConfigLoader.java
+++ src/main/java/io/rpg/config/ConfigLoader.java
@@ -139,7 +139,7 @@ public class ConfigLoader {
   }
 
   @NotNull
-  GameWorldConfig loadGameWorldConfig() throws FileNotFoundException {
+  private GameWorldConfig loadGameWorldConfig() throws FileNotFoundException {
     logger.info("Loading game world config");
     BufferedReader reader = new BufferedReader(new FileReader(pathToRootFile.toString()));
     GameWorldConfig config = gson.fromJson(reader, GameWorldConfig.class);
@@ -150,7 +150,7 @@ public class ConfigLoader {
     return config;
   }
 
-  LocationConfig loadLocationConfig(@NotNull String locationTag) throws FileNotFoundException {
+  private LocationConfig loadLocationConfig(@NotNull String locationTag) throws FileNotFoundException {
     logger.info("Loading location: " + locationTag);
 
     Path locationDir = pathToLocationsDir.resolve(locationTag);
@@ -173,7 +173,7 @@ public class ConfigLoader {
     return config;
   }
 
-  public void validate() {
+  private void validate() {
     if (!Files.isDirectory(pathToConfigDir)) {
       logger.error(ERR_INVALID_CFG_DIR_PATH);
       throw new IllegalArgumentException(ERR_INVALID_CFG_DIR_PATH);
