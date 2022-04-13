77279ff489ad94f0755e5e1f44c3b3b29b7d3672
Kacper Kafara <kacperkafara@gmail.com>
2022-04-13 16:20:13 +0200

    chore: add builder to the Game class

--- src/main/java/io/rpg/Game.java
+++ src/main/java/io/rpg/Game.java
@@ -2,11 +2,12 @@ package io.rpg;
 
 import io.rpg.controller.Controller;
 import javafx.scene.Scene;
+import org.jetbrains.annotations.NotNull;
 
 public class Game {
   private Controller controller;
 
-  public Game() {
+  private Game() {
 
   }
 
@@ -20,5 +21,24 @@ public class Game {
 
   public void setController(Controller controller) {
     this.controller = controller;
+  }
+
+  public static class Builder {
+    private final Game game;
+
+    public Builder() {
+      game = new Game();
+    }
+
+    public Game build() {
+      assert game.controller != null : "Attempt to build Game object without controller";
+      return game;
+    }
+
+    public Builder setController(Controller controller) {
+      assert controller != null : "Attempt to set null controller";
+      game.setController(controller);
+      return this;
+    }
   }
 }
