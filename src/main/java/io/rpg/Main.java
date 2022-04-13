2223fa02d29df7e6b25f8fe6b9c8ba364659362b
Kacper Kafara <kacperkafara@gmail.com>
2022-04-13 16:20:44 +0200

    refact: adapt Main to new Result based interface

--- src/main/java/io/rpg/Main.java
+++ src/main/java/io/rpg/Main.java
@@ -1,8 +1,11 @@
 package io.rpg;
 
+import io.rpg.util.Result;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.config.Configurator;
 
 import java.io.IOException;
@@ -11,8 +14,19 @@ public class Main extends Application {
   @Override
   public void start(Stage stage) throws IOException {
     Configurator.setRootLevel(Level.DEBUG);
+    Logger logger = LogManager.getLogger(Main.class);
+
     Initializer worldInitializer = new Initializer("configurations/config-1", stage);
-    Game game = worldInitializer.getWorld();
+    Result<Game, Exception>  initializationResult = worldInitializer.initialize();
+
+    if (initializationResult.isError()) {
+      logger.error("Initialization error");
+      // TODO: HANLDE BAD INIT
+    } else if (initializationResult.isOkValueNull()) {
+      // TODO: HANDLE
+    }
+
+    Game game = initializationResult.getOkValue();
 
     stage.setScene(game.getWorldView());
 
