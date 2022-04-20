448ab7e9c60e52f26cd6280a1fb3540b923ce56f
ksiek127 <ketain@interia.pl>
2022-04-20 10:34:33 +0200

    chore: attempt to display game objects on screen

---
+++ src/main/java/io/rpg/viewmodel/GameObjectViewModel.java
@@ -0,0 +1,33 @@
+package io.rpg.viewmodel;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GameObjectViewModel implements Initializable {
+    @FXML
+    private Pane imgPane;
+
+    @FXML
+    private ImageView gameObjectView;
+
+    public GameObjectViewModel() {
+    }
+
+    public void setImage(Image image){
+        this.gameObjectView.imageProperty().setValue(image);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        gameObjectView.imageProperty().addListener((property, oldImg, newImg) -> {
+            imgPane.setPrefWidth(newImg.getWidth());
+            imgPane.setPrefHeight(newImg.getHeight());
+        });
+    }
+}
