97d69d2c61b1f33bafb5754e4cef63b488e72d2a
Kacper Kafara <kacperkafara@gmail.com>
2022-04-23 10:47:44 +0200

    chore: add builder for LocationModel
    This aims to extract the creation logic to separate class to make object
    injection possible. (Object injection is needed as we need register view
        to model)

--- src/main/java/io/rpg/model/location/LocationModel.java
+++ src/main/java/io/rpg/model/location/LocationModel.java
@@ -3,6 +3,9 @@ package io.rpg.model.location;
 import io.rpg.model.data.LocationModelStateChange;
 import io.rpg.model.object.Player;
 import io.rpg.model.object.GameObject;
+import io.rpg.util.Result;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.UnmodifiableView;
 
@@ -19,9 +22,12 @@ public class LocationModel implements LocationModelStateChange.Emitter {
   private final Set<LocationModelStateChange.Observer> locationModelStateChangeObservers;
 
   public LocationModel(@NotNull String tag, @NotNull List<GameObject> gameObjects) {
+    this();
     this.tag = tag;
     this.gameObjects = gameObjects;
-    this.player = null;
+  }
+
+  private LocationModel() {
     this.locationModelStateChangeObservers = new LinkedHashSet<>();
   }
 
@@ -33,9 +39,29 @@ public class LocationModel implements LocationModelStateChange.Emitter {
     return tag;
   }
 
+  /**
+   * Private setter for Builder usage only.
+   *
+   * @param tag tag for the location
+   */
+  private void setTag(String tag) {
+    this.tag = tag;
+  }
+
   @UnmodifiableView
   public List<GameObject> getGameObjects() {
     return Collections.unmodifiableList(gameObjects);
+  }
+
+  /**
+   * Private setter for Builder usage only. Notice that ownership of {@link GameObject}s is not
+   * transferred to LocationModel.
+   * TODO: Transfer ownership of objects to LoactionModel.
+   *
+   * @param gameObjects game object for location
+   */
+  private void setGameObjects(List<GameObject> gameObjects) {
+    this.gameObjects = gameObjects;
   }
 
   @Override
@@ -53,5 +79,42 @@ public class LocationModel implements LocationModelStateChange.Emitter {
     locationModelStateChangeObservers.forEach(observer -> {
       observer.onLocationModelStateChange(event);
     });
+  }
+
+  public Result<Void, Void> validate() {
+    if (tag == null || gameObjects == null) {
+      return Result.error(null);
+    }
+    return Result.ok(null);
+  }
+
+  public static class Builder {
+    private final LocationModel locationModel;
+
+    private final Logger logger;
+
+    public Builder() {
+      logger = LogManager.getLogger(Builder.class);
+      locationModel = new LocationModel();
+    }
+
+    public void setGameObjects(@NotNull List<GameObject> gameObjects) {
+      locationModel.setGameObjects(gameObjects);
+    }
+
+    public void setTag(@NotNull String tag) {
+      locationModel.setTag(tag);
+    }
+
+    public LocationModel build() {
+      Result<Void, Void> result = locationModel.validate();
+
+      // TODO(@kkafar): Handle this in better way. Consider returning the result.
+      if (result.isError()) {
+        logger.error("Error occurred while building LocationModel.");
+      }
+
+      return locationModel;
+    }
   }
 }
