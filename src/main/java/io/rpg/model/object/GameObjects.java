a5c864c54e1e96d0e420e6000cafc52b46f395a6
Kacper Kafara <kacperkafara@gmail.com>
2022-04-25 14:14:34 +0200

    fix: update game object type validation

--- src/main/java/io/rpg/model/object/GameObjects.java
+++ src/main/java/io/rpg/model/object/GameObjects.java
@@ -5,11 +5,12 @@ import org.jetbrains.annotations.Nullable;
 import java.util.Set;
 import java.util.concurrent.Executors;
 
+// TODO: refactor way of validating {@link GameObject.Type}
 public class GameObjects {
   // this class is mean to only provide static methods
   private GameObjects() {}
 
-  public static Set<String> VALID_TYPES = Set.of("interactive", "navigable");
+  public static Set<String> VALID_TYPES = Set.of("navigable", "dialog", "collectible", "player");
 
   public static boolean isValidType(@Nullable String type) {
     return type != null && VALID_TYPES.contains(type);
