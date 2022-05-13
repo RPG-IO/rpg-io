29381a3b97c9a9848c9aa81632c7a1232964bbfd
co012 <co123@interia.eu>
2022-05-13 19:01:46 +0200

    chore: new constructor from Point2D

--- src/main/java/io/rpg/model/data/Position.java
+++ src/main/java/io/rpg/model/data/Position.java
@@ -1,19 +1,24 @@
 package io.rpg.model.data;
 
 import java.util.Objects;
+import javafx.geometry.Point2D;
 
 /**
  * Represents current position by holding row / col values.
  * This class can NOT be record due to some issues with
  * Gson library.
  */
-public class Position {
+public final class Position {
   public final int row;
   public final int col;
 
   public Position(int row, int col) {
     this.row = row;
     this.col = col;
+  }
+
+  public Position(Point2D point2D) {
+    this((int) Math.round(point2D.getY()), (int) Math.round(point2D.getX()));
   }
 
   public int getRow() {
