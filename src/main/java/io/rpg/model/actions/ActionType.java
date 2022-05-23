b926dd773e6e3a474c3204a7fd341b63097a72bd
Kacper Kafara <kacperkafara@gmail.com>
2022-05-23 12:11:49 +0200

    chore: add ActionType enum

---
+++ src/main/java/io/rpg/model/actions/ActionType.java
@@ -0,0 +1,32 @@
+package io.rpg.model.actions;
+
+import java.util.Optional;
+
+public enum ActionType {
+  Quiz("quiz"),
+  GameEnd("game-end"),
+  LocationChange("location-change"),
+  ShowDescription("show-description"),
+  Dialogue("dialogue");
+
+  private final String asString;
+
+  ActionType(String string) {
+    asString = string;
+  }
+
+  public String toString() {
+    return asString;
+  }
+
+  public Optional<ActionType> fromString(String action) {
+    switch (action) {
+      case "quiz" -> { return Optional.of(Quiz); }
+      case "game-end" -> { return Optional.of(GameEnd); }
+      case "location-change" -> { return Optional.of(LocationChange); }
+      case "show-description" -> { return Optional.of(ShowDescription); }
+      case "dialogue" -> { return Optional.of(Dialogue); }
+      default -> { return Optional.empty(); }
+    }
+  }
+}
