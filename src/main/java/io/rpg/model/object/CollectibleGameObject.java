c41303a099e5b33e0acd4ee8380267a44a150942
zamojtel <56871145+zamojtel@users.noreply.github.com>
2022-05-24 11:17:51 +0200

    Update src/main/java/io/rpg/model/object/CollectibleGameObject.java
    Co-authored-by: Marcin Hawryluk <70582973+mhawryluk@users.noreply.github.com>

--- src/main/java/io/rpg/model/object/CollectibleGameObject.java
+++ src/main/java/io/rpg/model/object/CollectibleGameObject.java
@@ -4,8 +4,8 @@ import io.rpg.model.data.Position;
 import org.jetbrains.annotations.NotNull;
 
 public final class CollectibleGameObject extends InteractiveGameObject {
-  public CollectibleGameObject(@NotNull String tag, @NotNull Position position,String assetPath) {
-    super(tag, position,assetPath);
+  public CollectibleGameObject(@NotNull String tag, @NotNull Position position, String assetPath) {
+    super(tag, position, assetPath);
   }
 
   @Override
