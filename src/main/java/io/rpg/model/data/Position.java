dce47cf4936ddb724b7f6f4223bd5febed62fb43
co012 <co123@interia.eu>
2022-05-31 19:51:19 +0200

    chore: neighborhood iterator

--- src/main/java/io/rpg/model/data/Position.java
+++ src/main/java/io/rpg/model/data/Position.java
@@ -1,6 +1,11 @@
 package io.rpg.model.data;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 import java.util.Objects;
+import java.util.function.Consumer;
 import javafx.geometry.Point2D;
 
 /**
@@ -19,14 +24,6 @@ public final class Position {
 
   public Position(Point2D point2D) {
     this((int) Math.round(point2D.getY()), (int) Math.round(point2D.getX()));
-  }
-
-  public int getRow() {
-    return row;
-  }
-
-  public int getCol() {
-    return col;
   }
 
   @Override
@@ -49,4 +46,51 @@ public final class Position {
   public int hashCode() {
     return Objects.hash(row, col);
   }
+
+  public Iterator<Position> getNeighborhoodIter(Position upperBound) {
+    return new NeighborhoodIter(this, upperBound);
+  }
+
+  private static class NeighborhoodIter implements Iterator<Position> {
+    private final ListIterator<Position> iterator;
+
+    private NeighborhoodIter(Position center, Position upperBound) {
+      List<Position> positions = new ArrayList<>(8);
+      for (int i = -1; i < 2; i++) {
+        for (int j = -1; j < 2; j++) {
+          int col = center.col + i;
+          int row = center.row + j;
+
+          if (col < 0 || row < 0) continue;
+          if (col >= upperBound.col || row >= upperBound.row) continue;
+          if (i == 0 && j == 0) continue;
+
+          positions.add(new Position(col, row));
+        }
+      }
+
+      iterator = positions.listIterator();
+    }
+
+    @Override
+    public boolean hasNext() {
+      return iterator.hasNext();
+    }
+
+    @Override
+    public Position next() {
+      return iterator.next();
+    }
+
+    @Override
+    public void remove() {
+      iterator.remove();
+    }
+
+    @Override
+    public void forEachRemaining(Consumer<? super Position> action) {
+      iterator.forEachRemaining(action);
+    }
+  }
+
 }
