7ff2829d64b2f9aed8ceaa7c381b1deea3671cd9
Kacper Kafara <kacperkafara@gmail.com>
2022-06-01 20:41:49 +0200

    refact: rename ConditionConfig -> ConditionConfigBundle

---
+++ src/main/java/io/rpg/config/model/ConditionConfigBundle.java
@@ -0,0 +1,23 @@
+package io.rpg.config.model;
+
+import com.kkafara.rt.Result;
+import io.rpg.model.actions.ConditionType;
+import org.jetbrains.annotations.Nullable;
+
+public class ConditionConfigBundle implements ConfigWithValidation {
+
+  @Nullable
+  private ConditionType type;
+
+  
+
+  @Nullable
+  public ConditionType getType() {
+    return type;
+  }
+
+  @Override
+  public Result<Void, Exception> validate() {
+    return Result.ok();
+  }
+}
