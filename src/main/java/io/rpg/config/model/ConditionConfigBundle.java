fc8e77d4a72bbfd88a9eeade3f7eedd0ab60faa3
Kacper Kafara <kacperkafara@gmail.com>
2022-06-01 20:47:46 +0200

    chore: add itemTag field & validation

--- src/main/java/io/rpg/config/model/ConditionConfigBundle.java
+++ src/main/java/io/rpg/config/model/ConditionConfigBundle.java
@@ -1,7 +1,9 @@
 package io.rpg.config.model;
 
+import com.google.gson.annotations.SerializedName;
 import com.kkafara.rt.Result;
 import io.rpg.model.actions.ConditionType;
+import io.rpg.util.ErrorMessageBuilder;
 import org.jetbrains.annotations.Nullable;
 
 public class ConditionConfigBundle implements ConfigWithValidation {
@@ -10,12 +12,31 @@ public class ConditionConfigBundle implements ConfigWithValidation {
   private ConditionType type;
 
   @Nullable
+  @SerializedName(value = "item-tag", alternate = {"itemTag"})
+  private String itemTag;
+
+  @Nullable
   public ConditionType getType() {
     return type;
   }
 
+  @Nullable
+  public String getItemTag() {
+    return itemTag;
+  }
+
   @Override
   public Result<Void, Exception> validate() {
-    return Result.ok();
+    ErrorMessageBuilder builder = new ErrorMessageBuilder();
+
+    if (type == null) {
+      builder.append("No or invalid type provided");
+    }
+
+    if (itemTag == null || itemTag.isBlank()) {
+      builder.append("No or invalid item tag provided");
+    }
+
+    return builder.isEmpty() ? Result.ok() : Result.err(new Exception(builder.toString()));
   }
 }
