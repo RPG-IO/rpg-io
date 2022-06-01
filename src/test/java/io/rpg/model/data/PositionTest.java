ba14782f180e17767d2d3372abeddb2b45465a02
co012 <co123@interia.eu>
2022-06-01 15:53:17 +0200

    chore: spacing

--- src/test/java/io/rpg/model/data/PositionTest.java
+++ src/test/java/io/rpg/model/data/PositionTest.java
@@ -10,17 +10,17 @@ class PositionTest {
 
   @Test
   void neighborhoodIterBasicTest() {
-    Position center = new Position(5,5);
-    Position upperBound = new Position(10,10);
+    Position center = new Position(5, 5);
+    Position upperBound = new Position(10, 10);
     List<Position> neighbors = List.of(
-        new Position(4,4),
-        new Position(4,5),
-        new Position(4,6),
-        new Position(5,4),
-        new Position(5,6),
-        new Position(6,4),
-        new Position(6,5),
-        new Position(6,6)
+        new Position(4, 4),
+        new Position(4, 5),
+        new Position(4, 6),
+        new Position(5, 4),
+        new Position(5, 6),
+        new Position(6, 4),
+        new Position(6, 5),
+        new Position(6, 6)
     );
 
     Iterator<Position> expectedIt = neighbors.iterator();
@@ -33,12 +33,12 @@ class PositionTest {
 
   @Test
   void neighborhoodIterUpperBoundTest() {
-    Position center = new Position(5,5);
-    Position upperBound = new Position(6,6);
+    Position center = new Position(5, 5);
+    Position upperBound = new Position(6, 6);
     List<Position> neighbors = List.of(
-        new Position(4,4),
-        new Position(4,5),
-        new Position(5,4)
+        new Position(4, 4),
+        new Position(4, 5),
+        new Position(5, 4)
     );
 
     Iterator<Position> expectedIt = neighbors.iterator();
@@ -51,12 +51,12 @@ class PositionTest {
 
   @Test
   void neighborhoodIterLowerBoundTest() {
-    Position center = new Position(0,0);
-    Position upperBound = new Position(10,10);
+    Position center = new Position(0, 0);
+    Position upperBound = new Position(10, 10);
     List<Position> neighbors = List.of(
-        new Position(0,1),
-        new Position(1,0),
-        new Position(1,1)
+        new Position(0, 1),
+        new Position(1, 0),
+        new Position(1, 1)
     );
 
     Iterator<Position> expectedIt = neighbors.iterator();
