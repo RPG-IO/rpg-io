b9a442250b96e074fd5f42e3f350e7d168b77da1
Kacper Kafara <kacperkafara@gmail.com>
2022-06-03 23:45:55 +0200

    chore: add DEFEAT_OPPONENT condition type

--- src/main/java/io/rpg/model/actions/ConditionType.java
+++ src/main/java/io/rpg/model/actions/ConditionType.java
@@ -4,5 +4,8 @@ import com.google.gson.annotations.SerializedName;
 
 public enum ConditionType {
   @SerializedName("item-required")
-  ITEM_REQUIRED
+  ITEM_REQUIRED,
+
+  @SerializedName("defeat-opponent")
+  DEFEAT_OPPONENT,
 }
