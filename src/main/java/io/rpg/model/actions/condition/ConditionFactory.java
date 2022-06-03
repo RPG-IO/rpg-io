4c77ec0c46d2ba7a6e51c853c47c959662442329
Kacper Kafara <kacperkafara@gmail.com>
2022-06-03 22:02:13 +0200

    chore: implement ConditionFactory

--- src/main/java/io/rpg/model/actions/condition/ConditionFactory.java
+++ src/main/java/io/rpg/model/actions/condition/ConditionFactory.java
@@ -1,5 +1,18 @@
 package io.rpg.model.actions.condition;
 
-public class ConditionFactory {
+import io.rpg.config.model.ConditionConfigBundle;
 
+public class ConditionFactory {
+  public static Condition fromConfig(ConditionConfigBundle config) {
+    // config was validated
+    //noinspection ConstantConditions
+    switch (config.getType()) {
+      case ITEM_REQUIRED -> { return itemRequiredFromConfig(config); }
+      default -> throw new IllegalArgumentException("Not implemented condition type: " + config.getType().toString());
+    }
+  }
+
+  private static ItemRequiredCondition itemRequiredFromConfig(ConditionConfigBundle config) {
+    return new ItemRequiredCondition();
+  }
 }
