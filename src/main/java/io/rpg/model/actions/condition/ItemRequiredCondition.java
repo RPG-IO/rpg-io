30c083305def8cf7616064adc48e97f3421c9e25
Kacper Kafara <kacperkafara@gmail.com>
2022-06-03 22:01:30 +0200

    feat: implement ItemRequiredCondition

--- src/main/java/io/rpg/model/actions/condition/ItemRequiredCondition.java
+++ src/main/java/io/rpg/model/actions/condition/ItemRequiredCondition.java
@@ -1,4 +1,25 @@
 package io.rpg.model.actions.condition;
 
+import io.rpg.model.actions.ConditionType;
+import org.jetbrains.annotations.NotNull;
+
 public class ItemRequiredCondition extends Condition {
+
+  @NotNull
+  private final String requiredItemTag;
+
+  public ItemRequiredCondition(@NotNull final String tag) {
+    super(ConditionType.ITEM_REQUIRED);
+    this.requiredItemTag = tag;
+  }
+
+  @Override
+  public boolean acceptEngine(ConditionEngine engine) {
+    return engine.evaluateItemRequiredCondition(this);
+  }
+
+  @NotNull
+  public String getItemTag() {
+    return requiredItemTag;
+  }
 }
