ccc7394dfc1ee8008598e6163c0ade5b2f61e0b5
Kacper Kafara <kacperkafara@gmail.com>
2022-06-09 22:20:31 +0200

    chore: update ConditionFactory

--- src/main/java/io/rpg/model/actions/condition/ConditionFactory.java
+++ src/main/java/io/rpg/model/actions/condition/ConditionFactory.java
@@ -16,6 +16,7 @@ public final class ConditionFactory {
       case ITEM_REQUIRED -> { return itemRequiredFromConfig(config); }
       case DEFEAT_OPPONENT -> { return defeatOpponentFromConfig(config); }
       case LEVEL_REQUIRED -> { return levelRequiredFromConfig(config); }
+      case POINTS_REQUIRED -> { return pointsRequiredFromConfig(config); }
       default -> throw new IllegalArgumentException("Not implemented condition type: " + config.getType().toString());
     }
   }
@@ -33,5 +34,10 @@ public final class ConditionFactory {
   private static LevelRequiredCondition levelRequiredFromConfig(ConditionConfigBundle config) {
     assert config.getRequiredLevel() != null;
     return new LevelRequiredCondition(config.getRequiredLevel());
+  }
+
+  private static PointsRequiredCondition pointsRequiredFromConfig(ConditionConfigBundle config) {
+    assert config.getRequiredPoints() != null;
+    return new PointsRequiredCondition(config.getRequiredPoints());
   }
 }
